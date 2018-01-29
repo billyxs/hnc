a8b2334e8a8699b98e8c1ac2500bf1dad4359f9e
Billy Montgomery <billy.montgomery@gmail.com>
2018-01-28 21:32:45 -0800

    go through links and prompt every 10

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+import inquirer from 'inquirer'
 import chalk from 'chalk'
 import Parser from 'rss-parser'
 
@@ -13,11 +14,34 @@ const getFeed = async () => {
   }
 }
 
+function logArticle(item) {
+  console.log(`${chalk.cyan(item.title)}
+${item.link}
+${chalk.white(item.pubDate)}`)
+}
+
+async function throttleWithPrompt(articles) {
+  try {
+    articles.slice(0, 10).forEach(logArticle)
+    const articlesLeft = articles.slice(10)
+
+    const { cont } = await inquirer.prompt({
+      type: 'confirm',
+      name: 'cont',
+      message: 'See more?',
+      when: articlesLeft.length,
+    })
+
+    return cont ?
+      throttleWithPrompt(articlesLeft) :
+      true
+  } catch (e) {
+    console.log(e)
+    return Promise.reject(e)
+  }
+}
+
 getFeed()
   .then((res) => {
-    console.log(`${chalk.white('------------------------------')}`)
-    res.items.forEach((item) => {
-      console.log(`${chalk.cyan(item.title)}
-${item.link}`)
-    })
+    throttleWithPrompt(res)
   })
